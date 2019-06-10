79e21ae89d4dbccb4f38e1f4882a2e6af9ca7591
Leonardo Salles <leonardosalles@leonardos-mbp.queroquero.local>
2019-06-10 15:05:04 -0300

    ajuste.

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 
+var http = require('http');
+var app = require('express')();
+
+var ip = process.env.IP || '0.0.0.0';
+var port = process.env.PORT || 8080;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(80, function () {
-  console.log('Example app listening on port 80!');
+http.createServer(app).listen(port, ip, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
